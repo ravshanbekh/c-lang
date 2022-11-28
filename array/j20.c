14f2738e055939a468344448920783bf66151d7c
RavshanbekIsroilov <isroilovravshan42@gmail.com>
2022-11-28 09:16:12 +0500

    .

---
+++ array/j20.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void palindrom(int nums[],int n){
+    int c=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]!=nums[n-1-i]){
+            printf("palindrom son emas");
+            c++;
+        }
+    }
+    if(c==0){
+        printf("palindrom son");
+    }
+
+}
+
+int main(){
+    int n;
+    printf("nechta qiymat kiritasiz: ");scanf("%d",&n);
+    int nums[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&nums[i]);
+    }
+    palindrom(nums,n);
+
+
+    return 0;
+}
